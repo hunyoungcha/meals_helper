50823aaee12dff51ff001fdd760050d79d2c8a4c
hunyoungcha <chamgf5247@naver.com>
2023-01-17 10:51:40 +0900

    2

--- crawling.py
+++ crawling.py
@@ -2,14 +2,17 @@
 #!/usr/bin/python3
 #-*-coding:utf-8-*-
 from urllib.request import urlopen
+from urllib import parse
 from bs4 import BeautifulSoup as bs
 
-html=urlopen(f"https://www.10000recipe.com/recipe/list.html?q={input().encode('utf-8')}")
+html=urlopen(f"https://www.10000recipe.com/recipe/list.html?q={parse.quote(input())}")
 obj=bs(html,"html.parser")
 
-print(obj)
-# for link in obj.find_all('a',class_="common_sp_link"):
-#     print(link.text.strip(), link.get('href'))
+f_list=obj.find_all(class_='common_sp_link') 
+
+print(f_list[0])
+
+
 
 # text_data=[]
 # for i in obj.select('.se-fs-' ):
