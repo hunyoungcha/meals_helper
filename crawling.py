d085484607b70212c0436a7a96b4c095b2e7ee67
hunyoungcha <chamgf5247@naver.com>
2023-01-21 20:42:16 +0900

    4

--- crawling.py
+++ crawling.py
@@ -4,18 +4,23 @@
 from urllib.request import urlopen
 from urllib import parse
 from bs4 import BeautifulSoup as bs
+from pprint import pprint
+import re
 
-html=urlopen(f"https://www.10000recipe.com/recipe/list.html?q={parse.quote(input())}")
-obj=bs(html,"html.parser")
+def get_number(menu_name, num=0):  #메뉴 이름을 입력 받으면 레시피 넘버를 넘겨줌
+    html=urlopen(f"https://www.10000recipe.com/recipe/list.html?q={parse.quote(menu_name)}")
+    obj=bs(html,"html.parser")
 
-f_list=obj.find_all(class_='common_sp_link') 
+    f_list=list(map(str,obj.find_all(class_='common_sp_link')))
+    pag_num=re.sub(r'[^0-9]','', f_list[num].split('\n')[0])  #레시피 넘버가 저장됨
+    return pag_num
 
-print(f_list[0])
-
-
-
-# text_data=[]
-# for i in obj.select('.se-fs-' ):
-#     if i.get_text()!='\u200b':
-#         text_data.append(i.get_text())
-# print(text_data)
+def get_recipe(pag_num): #음식의 재료를 리턴하는 함수
+    html=urlopen(f"https://www.10000recipe.com/recipe/{parse.quote(pag_num)}")
+    obj=bs(html,"html.parser")
+    
+    tmp=obj.find('div',{'id':'divConfirmedMaterialArea'}).find('ul').find_all('li')
+    f=[]
+    for i in tmp:
+        f.append(i.get_text().replace(' ','').split('\n')[0])
+    return f
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import tkinter as tk
+import crawling as cr
+
 
 window=tk.Tk()
 window.title('meals_helper')
 window.geometry('800x800')
 
+def btn_click():
+    outx.delete('1.0','end')
+    try:
+        outx.insert('1.0',cr.get_recipe(cr.get_number(inx.get())))
+    except:
+        #enctry위에 라벨 하나 만들건데 그 위에 잘못 입력되었다고 표시 하기
+
+inx=tk.Entry(window, width=100)
+btn=tk.Button(window, text='입력', width=10, height=3, command=btn_click)
+outx=tk.Text(window, width=40, height=10, font=10)
 
 
 
 
-
-
+inx.pack()
+btn.pack()
+outx.pack(side='left')
 window.mainloop()
